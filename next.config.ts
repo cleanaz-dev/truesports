fe56e7aee88fd344f22df55b1a2dc9b238a08b0a
CleanAZCoding <founder@cleanazcoding.com>
2026-08-09 14:48:16 -0400

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   images: {
+  images: {
     remotePatterns: [
-      { protocol: "https", hostname: "a.espncdn.com" }, // ESPN images
+      { protocol: "https", hostname: "*.espncdn.com" }, // covers a., a1-4., combiner., s., etc.
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
